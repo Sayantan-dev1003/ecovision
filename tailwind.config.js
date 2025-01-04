1a6cfc661d05582158faad782c6bb7389bef805b
Sayantan Halder <sayantanhalder78@gmail.com>
2025-01-04 08:35:04 +0530

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile': {'raw': '(min-width: 360px) and (min-height: 800px)'},
+        'tablet': {'raw': '(min-width: 600px) and (min-height: 1024px)'},
+        'laptop': {'raw': '(min-width: 1024px) and (min-height: 600px)'},
+      }
+    },
   },
   plugins: [],
 }
